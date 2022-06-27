8c5fc11f00df86cb5f442661aed596086d343784
Branden Ward <brandencward@gmail.com>
2022-06-27 10:29:36 -0500

    Add swagger as landing page. Rename Dev workflow.

--- Program.cs
+++ Program.cs
@@ -14,7 +14,11 @@ builder.Services.AddTransient<IReadEnvironmentVariable, ReadEnvironmentVariable>
 var app = builder.Build();
 
 app.UseSwagger();
-app.UseSwaggerUI();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("./swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
 
 app.UseHttpsRedirection();
 
